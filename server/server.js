dfb6a48fe206e61c654f82060fca1824762117de
Priyanshu Verma <priyanshuv360@gmail.com>
2026-09-08 00:55:54 +0530

    Fix CORS configuration

--- server/server.js
+++ server/server.js
@@ -14,7 +14,29 @@ connectDB()
 connectCloudinary();
 
 const app = express()
-app.use(cors()) // Enable Cross-origin resource sharing
+// app.use(cors()) // Enable Cross-origin resource sharing
+// app.use(cors({
+//     origin: "https://quickstay-dusky-one.vercel.app",
+//     credentials: true
+// }));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://quickstay-dusky-one.vercel.app"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
+
+
+
 
 // Middleware 
 app.use(express.json())
